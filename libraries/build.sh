39e597d459cf8b33aa5afaa8595961a733a0f17f
John Doe <yamukha@ukr.net>
2024-09-29 15:52:46 +0300

    fixed CI to build for Linux target for nlohnann json case

--- libraries/build.sh
+++ libraries/build.sh
@@ -24,7 +24,7 @@ fi
 if [[ "$JLIB" == "json" ]] # nlohmann::json 
 then
    # g++ esp_ecsda.c rlp/RLP.cpp sha3/sha3.cpp micro-ecc/uECC.c RpcEth/DefineEth.h RpcEth/hexutils.h RpcEth/ethsign.h RpcEth/JsonUtilsEth.h -o ecdsa -fpermissive -IRpcEth  -Irlp -Isha3 -Imicro-ecc -DUSE_LINUX -DDEBUG_PRINT -DUSE_JSON
-   g++ esp_ecsda.c rlp/RLP.cpp sha3/sha3.cpp micro-ecc/uECC.c RpcEth/DefineEth.h RpcEth/hexutils.h RpcEth/ethsign.h RpcEth/JsonUtilsEth.h secp256k1-embedded/src/ethc.cpp -o ecdsa -fpermissive -lsecp256k1 -IRpcEth  -Irlp -Isha3 -Imicro-ecc -DUSE_LINUX -DDEBUG_PRINT -DUSE_JSON -DDEBUG_PRINT
+   g++ esp_ecsda.c rlp/RLP.cpp sha3/sha3.cpp micro-ecc/uECC.c RpcEth/DefineEth.h RpcEth/hexutils.h RpcEth/ethsign.h RpcEth/JsonUtilsEth.h secp256k1-embedded/src/ethc.cpp -o ecdsa -fpermissive -lsecp256k1 -IRpcEth  -Irlp -Isha3  -Isecp256k1-embedded/src/include -Imicro-ecc -DUSE_LINUX -DDEBUG_PRINT -DUSE_JSON -DDEBUG_PRINT
 fi
 
 ls -la ./ecdsa
