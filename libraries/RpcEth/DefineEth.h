17aa384a3eed1ee4dc5c72c82630aef9bc93dc39
John Doe <yamukha@ukr.net>
2024-09-29 15:31:29 +0300

    fixed CI to build ESP with json11 as default library

--- libraries/RpcEth/DefineEth.h
+++ libraries/RpcEth/DefineEth.h
@@ -1,8 +1,11 @@
 #ifndef DEFINE_ETH_H
 #define DEFINE_ETH_H
 
-// #define USE_JSON11
+#ifndef USE_LINUX
+#define USE_JSON11
 // #define USE_JSON
+#endif
+
 #ifdef USE_JSON11
 #define USE_JSON_OR_JSON11
 #undef USE_JSON
