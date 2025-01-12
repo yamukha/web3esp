6abb6d9c7f7808bff5b3b0a476353c858019792d
am <yamukha@ukr.net>
2025-01-12 16:11:19 +0200

    try to fix action linux build with #include <cstdint>

--- libraries/RpcEth/hexutils.h
+++ libraries/RpcEth/hexutils.h
@@ -6,15 +6,18 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <cstdint>
 
 #include <DefineEth.h>
 
-std::vector<uint8_t> hex2bytes (std::string hex) {
+std::vector<uint8_t> hex2bytes(std::string hex)
+{
     std::vector<uint8_t> bytes;
-    for (unsigned int i = 0; i < hex.length(); i += 2) {
-      std::string byteString = hex.substr(i, 2);
-      uint8_t byte = (uint8_t) strtol(byteString.c_str(), NULL, 16);
-      bytes.push_back(byte);
+    for (unsigned int i = 0; i < hex.length(); i += 2)
+    {
+        std::string byteString = hex.substr(i, 2);
+        uint8_t byte = (uint8_t)strtol(byteString.c_str(), NULL, 16);
+        bytes.push_back(byte);
     }
     return bytes;
 }
